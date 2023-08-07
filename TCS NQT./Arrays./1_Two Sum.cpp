0d0eb14ae2c3231db123272786a276285bc16143
S. Manna <90498811+codewar193@users.noreply.github.com>
2023-08-08 00:11:58 +0530

    LeetCode #easy

---
+++ TCS NQT./Arrays./1_Two Sum.cpp
@@ -0,0 +1,43 @@
+//Method -1
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+        vector<int> ans;
+        unordered_map <int,int> mp;
+        
+        int n =nums.size();
+        for(int i=0;i<n;i++){
+            if(mp.find(target - nums[i])!=mp.end()){
+                ans.push_back(mp[target-nums[i]]);
+                ans.push_back(i);
+                return ans;
+            }
+            mp[nums[i]]=i;
+        }
+        return ans;
+    }
+};
+
+
+//Method -2 
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int>ans;
+
+        for(int i =0;i<nums.size();i++){
+            int temp = target- nums[i];
+
+            for(int j =i+1;j<nums.size();j++){
+                  if(nums[j]==temp){
+                      ans.push_back(i);
+                      ans.push_back(j);
+                  }
+            }
+        }
+        return ans;
+    }
+};
